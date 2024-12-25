2863f7f1ca4270037a9a921823424bd977f4c724
shubham mishra <mishrashyam1968@gmail.com>
2024-12-25 04:15:48 -0800

    learner

--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Footer from '../../Components/Footer/Footer'
-import Banner from '../../Components/Banner/banner'
+import Banner from '../../Components/Banner/Banner'
 import Hero from '../../Components/Hero/Hero'
 import Explore from '../../Components/Explore/Explore'
 import  Card  from '../../Components/Card/Card'
